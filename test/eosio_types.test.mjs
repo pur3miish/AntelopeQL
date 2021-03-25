5e047333ee08de6d3ff12556d717cdfe97386f36
H <pur3miish@me.com>
2021-03-25 23:16:45 +1000

    Create eosio_types.test.mjs

---
+++ test/eosio_types.test.mjs
@@ -0,0 +1,113 @@
+import { deepStrictEqual, ok, rejects, strictEqual, throws } from 'assert'
+import asset_type from '../private/eos_types/asset_type.js'
+import block_time_stamp from '../private/eos_types/block_timestamp_type.js'
+import bool from '../private/eos_types/boolean_type.js'
+import bytes from '../private/eos_types/bytes_type.js'
+import extended_asset from '../private/eos_types/extended_asset_type.js'
+import generate_checksum from '../private/eos_types/generate_checksum_type.js'
+import generate_float_type from '../private/eos_types/generate_float_type.js'
+import generate_int_type from '../private/eos_types/generate_int_type.js'
+import generate_uint_type from '../private/eos_types/generate_uint_type.js'
+import name from '../private/eos_types/name_type.js'
+import public_key from '../private/eos_types/public_key_type.js'
+import signature from '../private/eos_types/signature_type.js'
+import symbol_code from '../private/eos_types/symbol_code_type.js'
+import symbol from '../private/eos_types/symbol_type.js'
+import time_point_sec from '../private/eos_types/time_point_sec_type.js'
+import time_point from '../private/eos_types/time_point_type.js'
+import varint32 from '../private/eos_types/varint32_type.js'
+import varuint32 from '../private/eos_types/varuint32_type.js'
+
+/*  private/eos_types/generate_checksum.js:18:17 → 25:5
+private/eos_types/mutation_types.js:13:11 → 25:4
+private/eos_types/mutation_types.js:50:11 → 92:4
+*/
+export default tests => {
+  // These tests are very basic, I plan to integrate the AST into the EOS types.
+  // these tests are more of a placeholder until the concatenation of the serilization and EOS types complete.
+  tests.add('EOSIO types - validating parse values', async () => {
+    deepStrictEqual(varint32.parseValue(''), '')
+    deepStrictEqual(varuint32.parseValue(''), '')
+    strictEqual(time_point.parseValue('1616667468'), '1616667468')
+    strictEqual(time_point_sec.parseValue('1616667453647'), '1616667453647')
+
+    deepStrictEqual(symbol.parseValue('3,EOS'), '3,EOS')
+    deepStrictEqual(symbol_code.parseValue('EOS'), 'EOS')
+    deepStrictEqual(
+      signature.parseValue(
+        'SIG_K1_K4jhCs4S3hVfXNhX4t6QSSGgdYTYNk6LhKTphcYoLH6EYatq3zvU38CNEj7VDtMmHWq24KhmR6CLBqyT5tFiFmXndthr7X'
+      ),
+      'SIG_K1_K4jhCs4S3hVfXNhX4t6QSSGgdYTYNk6LhKTphcYoLH6EYatq3zvU38CNEj7VDtMmHWq24KhmR6CLBqyT5tFiFmXndthr7X'
+    )
+    ok(signature.parseValue('') == '')
+
+    throws(() =>
+      signature.parseValue(
+        'NOT_SIG_K4jhCs4S3hVfXNhX4t6QSSGgdYTYNk6LhKTphcYoLH6EYatq3zvU38CNEj7VDtMmHWq24KhmR6CLBqyT5tFiFmXndthr4x'
+      )
+    )
+    throws(() => signature.parseValue(1848))
+
+    throws(() => asset_type.parseValue('#'))
+    ok(asset_type.parseValue('') == '')
+    strictEqual(asset_type.parseValue('1.000 EOS'), '1.000 EOS')
+    deepStrictEqual(block_time_stamp.parseValue(''), '')
+    throws(() => block_time_stamp.parseValue('not a time'))
+    deepStrictEqual(
+      block_time_stamp.parseValue('2021-03-01T12:26:42.147'),
+      '2021-03-01T12:26:42.147'
+    )
+    deepStrictEqual(bool.parseValue(true), true)
+    ok(bytes.parseValue('ff') == '02ff')
+    throws(() => bytes.parseValue('kl'))
+    ok(bytes.parseValue('') == '')
+    deepStrictEqual(
+      extended_asset.parseValue('1.000 EOS@eosio'),
+      '1.000 EOS@eosio'
+    )
+    deepStrictEqual(
+      generate_checksum(20).parseValue(
+        'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'
+      ),
+      'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'
+    )
+    ok(generate_float_type(32).parseValue('1.0'), '1.0')
+    deepStrictEqual(generate_int_type(8).parseValue(127), '127')
+    deepStrictEqual(generate_int_type(8).parseValue(-128), '-128')
+    throws(() => generate_int_type(8).parseValue(-129))
+    throws(() => generate_int_type(8).parseValue(128))
+    ok(generate_int_type(8).parseValue('') == '')
+    deepStrictEqual(generate_uint_type(8).parseValue(0), '0')
+    deepStrictEqual(generate_uint_type(8).parseValue(255), '255')
+    deepStrictEqual(generate_uint_type(16).parseValue(0xffff), '65535')
+    throws(() => generate_uint_type(16).parseValue(0x10000))
+    throws(() => generate_uint_type(8).parseValue(-1))
+    ok(generate_uint_type(8).parseValue('') == '')
+    deepStrictEqual(name.parseValue('eosio'), 'eosio')
+    deepStrictEqual(name.parseValue(''), '')
+    throws(() => name.parseValue('NOT A NAME'))
+    deepStrictEqual(
+      'EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV',
+      await public_key.parseValue(
+        'EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV'
+      )
+    )
+    deepStrictEqual('', await public_key.parseValue(''))
+
+    rejects(
+      () =>
+        public_key.parseValue(
+          'EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW533'
+        ),
+      'expected invalid checksum'
+    )
+
+    rejects(
+      () =>
+        public_key.parseValue(
+          'NOT_EOS_6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW533'
+        ),
+      'Expected invalid prefix'
+    )
+  })
+}
