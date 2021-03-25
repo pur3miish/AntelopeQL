1a7259231d264801ec2694bf198f271d50a21525
H <pur3miish@me.com>
2021-03-26 01:18:15 +1000

    More test coverage.

--- test/build_schema.test.js
+++ test/build_schema.test.js
@@ -1,12 +1,14 @@
 'use strict'
-const { ok } = require('assert')
+const { ok, throws } = require('assert')
 const { validateSchema } = require('graphql')
+const query_fields = require('../private/build_schema/build_query_fields/index.js')
 const build_schema = require('../private/build_schema/index.js')
+const base_abi = require('./abis/base_error.json')
 const EOS_ABI = require('./abis/eosio.json')
 const EOS_TOKEN_ABI = require('./abis/eosio.token.json')
 
 module.exports = tests => {
-  tests.add('schema snapshot - eosio.token', async () => {
+  tests.add('build schema', async () => {
     // check if valid schema.
     ok(
       !validateSchema(build_schema(EOS_ABI, 'eosio')).length,
@@ -17,5 +19,9 @@ module.exports = tests => {
       !validateSchema(build_schema(EOS_TOKEN_ABI, 'eosio.token')).length,
       'valid schema expected for eosio.token abi'
     )
+
+    ok(query_fields({}).noquery, 'no query')
+    throws(() => build_schema(null))
+    throws(() => build_schema(base_abi, 'base'))
   })
 }
