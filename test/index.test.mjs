039c3fcb95086dd686b1044f8603691aa5101cce
H <pur3miish@me.com>
2021-08-14 18:14:26 +1000

    Update index.test.mjs

--- test/index.test.mjs
+++ test/index.test.mjs
@@ -1,9 +1,8 @@
 import TestDirector from 'test-director'
-import network from './network.test.mjs'
 import wasm from './wasm.test.mjs'
 
 const tests = new TestDirector()
 // TODO need to add more tests for schema validation.
-network(tests)
+
 wasm(tests)
 tests.run()
