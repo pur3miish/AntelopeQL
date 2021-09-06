3ba296a7606cc7c4f75c288d1851f3033874f9f7
H <pur3miish@me.com>
2021-09-06 18:25:16 +1000

    removed redundant import.

--- private/build_schema/build_query_fields/index.js
+++ private/build_schema/build_query_fields/index.js
@@ -6,7 +6,6 @@ const {
   GraphQLNonNull,
   GraphQLInt
 } = require('graphql')
-const name = require('../../eos_types/name_type')
 const get_table_rows = require('../../network/get_table_rows')
 const abi_to_ast = require('../abi_to_ast/index.js.js')
 const generate_table_scope = require('./table_scope')
