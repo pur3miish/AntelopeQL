79a1c7f12d8a2560b88a399637e8225522069610
pur3miish <pur3miish@me.com>
2022-01-23 16:57:25 +1000

    v7.0.0

--- test/transaction.test.mjs
+++ test/transaction.test.mjs
@@ -10,7 +10,7 @@ const mutation = /* GraphQL */ `
 
 SmartQL({
   query: mutation,
-  contract: 'relocke',
+  contract: ['relocke'],
   rpc_url: 'http://127.0.0.1:8888',
   broadcast: true,
   private_keys: ['5K7xR2C8mBzMo4aMPJyBPp7Njc3XvszeJSfTApa51rc2d54rrd3']
