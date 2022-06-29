4d3ddaa1e34a4622ef01611535b4fdd6944d0f9b
H <pur3miish@me.com>
2022-06-29 20:59:48 +1000

    Update index.test.mjs

--- test/index.test.mjs
+++ test/index.test.mjs
@@ -1,7 +1,8 @@
 import TestDirector from 'test-director'
+import serialize_transactions from './serialize_transaction.test.mjs'
 import smartql from './smartql.test.mjs'
 
 const tests = new TestDirector()
-
-smartql(tests)
+serialize_transactions(tests)
+// smartql(tests)
 tests.run()
