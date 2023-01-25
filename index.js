3b21c36f692349288290e318fd0c686fbccff92d
H <pur3miish@me.com>
2023-01-25 20:16:02 +0000

    Added index.js for destructured imports

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+exports.build_graphql_fields_from_abis = require('./build_graphql_fields_from_abis.js')
+exports.antelope_abi_to_graphql_ast = require('./antelope_abi_to_graphql_ast.js')
+exports.mutation_resolver = require('./mutation_resolver.js')
+exports.push_serialized_transaction = require('./push_serialized_transaction.js')
+exports.push_transaction_rpc = require('./push_transaction_rpc.js')
+exports.push_transaction = require('./push_transaction.js')
+exports.query_resolver = require('./query_resolver.js')
+exports.serialize_transaction = require('./serialize_transaction.js')
+exports.smartql = require('./smartql.js')
