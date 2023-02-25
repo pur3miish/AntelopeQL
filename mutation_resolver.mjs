933e90cb75b9c48fe163c4fc3f9a06bd6ebfb9da
H <pur3miish@me.com>
2023-02-26 08:26:37 +1000

    import change to deep imports.

--- mutation_resolver.mjs
+++ mutation_resolver.mjs
@@ -1,4 +1,4 @@
-import serialize from "eosio-wasm-js";
+import serialize from "eosio-wasm-js/serialize.mjs";
 import serialize_transaction_header from "eosio-wasm-js/transaction_header.mjs";
 import { GraphQLError } from "graphql";
 
