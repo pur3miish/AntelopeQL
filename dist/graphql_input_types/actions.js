526bdc5e14797daed59400a0dc94bf3cbfcd5953
H <pur3miish@me.com>
2025-07-12 02:12:01 +1000

    removed dist.

--- dist/graphql_input_types/actions.js
+++
@@ -1,7 +0,0 @@
-import { GraphQLInputObjectType, GraphQLList, GraphQLNonNull } from "graphql";
-const actions_type = (fields, typeResolution = "") => new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(new GraphQLInputObjectType({
-    name: "actions_type" + typeResolution,
-    fields
-}))));
-export default actions_type;
-//# sourceMappingURL=actions.js.map
